b48a0a5ed727dddc6604baa13fd4803ef4c516bd
zcc <386927642@qq.com>
2017-07-31 21:52:09 +0800

    20170731

---
+++ depotapp/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from views import *
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    (r'product/list/$',product_list),
+    (r'product/create/$',create_product),
+    (r'product/edit/(?P<id>[^/]+)/$',edit_product),
+    (r'product/view/(?P<id>[^/]+)/$',view_product),
+
+)
--- depotapp/views.py
+++ depotapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import request,HttpResponse
+from depotapp.models import Product
+from django.template import loader,Context,RequestContext
+from django import forms
+# -*- coding:utf-8 -*-
+
+
 
 # Create your views here.
+
+def product_list(request):
+    product_list=Product.objects.all()
+    t=loader.get_template('product_list.html')
+    html=t.render(Context({"product_list":product_list}))
+    return HttpResponse(html)
+
+def create_product(request,id):
+    return '1'
+
+def view_product(request,id):
+    assert False
+    product_instance=Product.objects.get(id=id)
+    t=loader.get_template('view_product.html')
+    c=RequestContext(request,locals())
+    return HttpResponse(t.render(c))
+
+def edit_product(request,id):
+    return '1'
